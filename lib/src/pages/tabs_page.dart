0573a736accfd108855802da40fb10c6f1a6a601
Leonardo <leopg17@gmail.com>
2020-10-08 13:48:21 -0600

    Cambiando el PageView

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -36,7 +36,9 @@ class _Navegacion extends StatelessWidget {
 class _Paginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
     return PageView(
+      controller: navegacionModel.pageController,
       // physics: BouncingScrollPhysics(),
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
@@ -53,11 +55,17 @@ class _Paginas extends StatelessWidget {
 
 class _NavegacionModel with ChangeNotifier {
   int _paginaActual = 0;
+  PageController _pageController = new PageController();
 
   int get paginaActual => this._paginaActual;
 
   set paginaActual(int valor) {
     this._paginaActual = valor;
+
+    _pageController.animateToPage(valor,
+        duration: Duration(milliseconds: 250), curve: Curves.easeInOut);
     notifyListeners();
   }
+
+  PageController get pageController => this._pageController;
 }
