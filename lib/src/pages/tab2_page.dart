83255c1de47c7fd985d54f08e6f86e987b7a632a
Leonardo <leopg17@gmail.com>
2020-10-18 20:58:41 -0600

    Lista de categorias

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/services/news_service.dart';
+import 'package:provider/provider.dart';
 
 class Tabs2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Hola Mundo'),
-      ),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Expanded(child: _ListaCategorias())
+          ],
+        )
+      )
+    );
+  }
+}
+
+class _ListaCategorias extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = Provider.of<NewsService>(context).categories;
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index){
+        return Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              Icon(categories[index].icon),
+              SizedBox(height: 5,),
+              Text(categories[index].name)
+            ],
+          ),
+        );
+        //Text(categories[index].name);
+      }
     );
   }
 }
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,13 +10,25 @@ final _APIKEY = '1608631c0bf74462a4e06ebc9697aa4a';
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
 
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
+
+  //business entertainment general health science sports technology
+
   NewsService() {
     this.getTopHeadlines();
   }
 
   getTopHeadlines() async {
     //print('Cargando headlines...');
-    final url = '$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=us';
+    final url = '$_URL_NEWS/top-headlines?apiKey=$_APIKEY&language=es&country=us';
     final resp = await http.get(url);
 
     final newsResponse = newsResponseFromJson(resp.body);
