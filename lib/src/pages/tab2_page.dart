e1fcdec280ffedd0dfe38bd03287f0db66a9f5c2
Leonardo <leopg17@gmail.com>
2020-10-18 21:35:26 -0600

    Puliendo el menu de categorias

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/services/news_service.dart';
 import 'package:provider/provider.dart';
 
@@ -28,18 +29,49 @@ class _ListaCategorias extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: categories.length,
       itemBuilder: (BuildContext context, int index){
-        return Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: <Widget>[
-              Icon(categories[index].icon),
-              SizedBox(height: 5,),
-              Text(categories[index].name)
-            ],
+
+        final cName = categories[index].name;
+        return Container(
+          width: 110,
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                _CategoryButton(categories[index]),
+                SizedBox(height: 5,),
+                Text('${cName[0].toUpperCase()}${cName.substring(1)}')
+              ],
+            ),
           ),
         );
         //Text(categories[index].name);
       }
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  final Category categoria;
+  const _CategoryButton(this.categoria);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        print('${categoria.name}');
+      },
+          child: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white
+        ),
+        child: Icon(
+          categoria.icon,
+          color: Colors.black54,
+        ),
+      ),
     );
   }
 }
